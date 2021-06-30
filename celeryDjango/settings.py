e468621b30cf8b33c143a9038f6790db32ae56b7
Suyash More <63491387+suyash-more@users.noreply.github.com>
2021-06-30 16:57:34 +0530

    applied cache db in backends

--- celeryDjango/settings.py
+++ celeryDjango/settings.py
@@ -122,14 +122,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = 'default'
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-#         'LOCATION': 'my_cache_table',
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
 
 
 # Static files (CSS, JavaScript, Images)
